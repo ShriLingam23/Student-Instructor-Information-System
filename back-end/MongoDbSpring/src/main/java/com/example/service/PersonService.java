893600b614846361dd4a4562e8f7c7daf606009e
ShriLingam23 <trnshri23@gmail.com>
2019-06-24 16:17:54 +0530

    Springboot backend user crud

---
+++ back-end/MongoDbSpring/src/main/java/com/example/service/PersonService.java
@@ -0,0 +1,47 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.model.Person;
+import com.example.repository.PersonRepository;
+
+@Service
+public class PersonService {
+	
+	@Autowired
+	private PersonRepository personRepository;
+
+	// Create operation
+	public Person create(String firstName, String lastName, int age) {
+		return personRepository.save(new Person(firstName, lastName, age));
+	}
+
+	// Retrieve operation
+	public List<Person> getAll() {
+		return personRepository.findAll();
+	}
+
+	public Person getByFirstName(String firstName) {
+		return personRepository.findByFirstName(firstName);
+	}
+
+	// Update operation
+	public Person update(String firstName, String lastName, int age) {
+		Person p = personRepository.findByFirstName(firstName);
+		p.setLastName(lastName);
+		p.setAge(age);
+		return personRepository.save(p);
+	}
+
+	// Delete operation
+	public void deleteAll() {
+		personRepository.deleteAll();
+	}
+
+	public void delete(String firstName) {
+		Person p = personRepository.findByFirstName(firstName);
+		personRepository.delete(p);
+	}
+}
